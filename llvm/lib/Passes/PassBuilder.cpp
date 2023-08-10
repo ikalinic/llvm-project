ed27082b7dcc6086a3646ff39ec33b05ba348a31
Ilija Kalinić <ilija.kalinic@syrmia.com>
2023-08-10 13:09:24 +0200

    Registers new passes and updates CMakeLists.txt

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -235,6 +235,8 @@
 #include "llvm/Transforms/Utils/EntryExitInstrumenter.h"
 #include "llvm/Transforms/Utils/FixIrreducible.h"
 #include "llvm/Transforms/Utils/HelloWorld.h"
+#include "llvm/Transforms/Utils/DebugCount.h"
+#include "llvm/Transforms/Utils/RemoveDebugInst.h"
 #include "llvm/Transforms/Utils/InjectTLIMappings.h"
 #include "llvm/Transforms/Utils/InstructionNamer.h"
 #include "llvm/Transforms/Utils/LCSSA.h"
